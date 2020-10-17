09a677a82b20108fcb76f981a267e2f70641d146
ilja <ilja@kali.ylyxa>
2020-10-17 16:05:45 -0400

    Scripted auto-commit on change (2020-10-17 16:05:45) by gitwatch.sh

--- src/main/java/FirstComparator.java
+++ src/main/java/FirstComparator.java
@@ -1,8 +1,10 @@
 import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class FirstComparator implements RawComparator<TextPair> {
+public class FirstComparator extends WritableComparator {
     @Override
-    public int compare(TextPair o1, TextPair o2) {
-        return o1.getFirst().equals(o2.getFirst());
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        return ((TextPair)o1).getFirst().compareTo(o2.getFirst());
     }
 }
