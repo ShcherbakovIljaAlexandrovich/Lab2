0fd08c6db25dcc33666778f615c42f8fb0b956d8
Ilja <ilja@ylyxa.ylyxa.com>
2020-10-17 11:15:04 +0300

    Scripted auto-commit on change (2020-10-17 11:15:04) by gitwatch.sh

--- src/main/java/AirportsJoinMapper.java
+++ src/main/java/AirportsJoinMapper.java
@@ -1,2 +1,13 @@
-public class AirportsJoinMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text>{
+    @Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+
+    }
 }
