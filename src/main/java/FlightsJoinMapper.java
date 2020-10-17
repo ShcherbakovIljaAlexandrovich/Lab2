c29acdcf4b9b3bd1d98e09fb6c8d90af7de7c283
ilja <ilja@kali.ylyxa>
2020-10-17 18:29:51 -0400

    Scripted auto-commit on change (2020-10-17 18:29:51) by gitwatch.sh

--- src/main/java/FlightsJoinMapper.java
+++ src/main/java/FlightsJoinMapper.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        final int AIRPORT_COLUMN = 14;
+        final int DELAY_COLUMN = 18;
         String line = value.toString();
         if (line.contains("AIRPORT")) return;
         String[] tokens = line.split(",");
-        String airportID = tokens[14];
-        String delay = tokens[18];
+        String airportID = tokens[AIRPORT_COLUMN];
+        String delay = tokens[DELAY_COLUMN];
+        if (delay.equals("")) return;
         context.write(new TextPair(airportID, "1"), new Text(delay));
     }
 }
--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -11,17 +11,17 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
         Iterator<Text> iter = values.iterator();
         Text airportName = new Text(iter.next());
         if (!iter.hasNext()) return;
-        int min = Integer.parseInt(iter.next().toString());
-        int max = min;
+        float min = Float.parseFloat(iter.next().toString());
+        float max = min;
         float sum = min;
         int n = 1;
         while (iter.hasNext()) {
-            int cur = Integer.parseInt(iter.next().toString());
+            float cur = Float.parseFloat(iter.next().toString());
             if (cur<min) min = cur;
             if (cur>max) max = cur;
             sum += cur;
             n++;
         }
-        if (max>0) context.write(airportName, new Text(String.format("min:%d max:%d avg:%f", min, max, sum/n)));
+        if (max>0) context.write(airportName, new Text(String.format("min:%f max:%f avg:%f", min, max, sum/n)));
     }
 }
