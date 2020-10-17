1b4aab5551c9a1f472ced92f3a20afb32372b492
Ilja <ilja@ylyxa.ylyxa.com>
2020-10-17 11:09:08 +0300

    Scripted auto-commit on change (2020-10-17 11:09:08) by gitwatch.sh

--- src/main/java/AirportsJoinMapper.java
+++ src/main/java/AirportsJoinMapper.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class AirportsJoinMapper {
+public class AirportsJoinMapper {
 }
--- src/main/java/AvgDelayCalculator.java
+++ src/main/java/AvgDelayCalculator.java
@@ -1,2 +1,26 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class AvgDelayCalculator {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: AvgDelayCalculator <input path airports> <input path flights> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(AvgDelayCalculator.class);
+        job.setJobName("Calculate avg delay");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
