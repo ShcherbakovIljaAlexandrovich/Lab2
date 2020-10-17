35f3b3b6874887962aac2d3815832b7854f56214
ilja <ilja@kali.ylyxa>
2020-10-17 16:00:35 -0400

    Scripted auto-commit on change (2020-10-17 16:00:35) by gitwatch.sh

--- src/main/java/FirstComparator.java
+++ src/main/java/FirstComparator.java
@@ -1,5 +1,8 @@
 import org.apache.hadoop.io.RawComparator;
 
 public class FirstComparator implements RawComparator<TextPair> {
-    
+    @Override
+    public int compare(TextPair o1, TextPair o2) {
+        return o1.getFirst().equals(o2.getFirst());
+    }
 }
